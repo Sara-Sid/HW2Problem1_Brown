adcb18ce4d4be960fe1c2ce0ffafbfc955dc86e2
studentlab <s.brown28@ncf.edu>
2025-11-10 15:07:29 -0500

    go

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args){
+        HurricanePredictionTemplate machineLearningPrediction = new MachineLearningPrediction();
+        HurricanePredictionTemplate statisticalPrediction = new StatisticalPrediction();
+        WeatherDataFetcher fetcher = new WeatherDataFetcher();
+        ResultSaver results = new ResultSaver();
+        PredictionModel predicts = new PredictionModel();
+
+        //Create Commands
+        Command fetchData = new FetchDataCommand(FetchDataCommand fetcher);
+        Command pred
+
+
+        System.out.println("Machine Learning Prediction: ");
+        machineLearningPrediction.fetchData();
+        machineLearningPrediction.preprocessData();
+        machineLearningPrediction.applyPredictionModel();
+        machineLearningPrediction.postprocessResults();
+
+        System.out.println("Statistical Prediction: ");
+        statisticalPrediction.fetchData();
+        statisticalPrediction.preprocessData();
+        statisticalPrediction.applyPredictionModel();
+        statisticalPrediction.postprocessResults();
+
+    }
+}
